a52709f344f255499c65c1784f32a7edca587c0a
Mathis Engelbart <mathis.engelbart@gmail.com>
2025-03-22 21:29:31 +0800

    Move GCC rewrite from interceptor to this repo

---
+++ gcc/rtt.go
@@ -0,0 +1,8 @@
+package gcc
+
+import "time"
+
+func MeasureRTT(reportSent, reportReceived, latestAckedSent, latestAckedArrival time.Time) time.Duration {
+	pendingTime := reportSent.Sub(latestAckedArrival)
+	return reportReceived.Sub(latestAckedSent) - pendingTime
+}
