4dcb4076f28a88e7b0b5343b3efe54b09831835f
Mathis Engelbart <mathis.engelbart@gmail.com>
2025-09-27 10:18:13 +0200

    Add synctest test

---
+++ vnet/vnet_test.go
@@ -0,0 +1,60 @@
+// SPDX-FileCopyrightText: 2025 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"testing"
+	"testing/synctest"
+
+	"github.com/pion/logging"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVnet(t *testing.T) {
+	lf := logging.NewDefaultLoggerFactory()
+	logger := lf.NewLogger("bwe_vnet_synctest")
+
+	testCases := []struct {
+		name       string
+		senderMode senderMode
+		flowMode   flowMode
+	}{
+		{
+			name:       "TestVnetRunnerABR/VariableAvailableCapacitySingleFlow",
+			senderMode: abrSenderMode,
+			flowMode:   singleFlowMode,
+		},
+		{
+			name:       "TestVnetRunnerABR/VariableAvailableCapacityMultipleFlows",
+			senderMode: abrSenderMode,
+			flowMode:   multipleFlowsMode,
+		},
+		{
+			name:       "TestVnetRunnerSimulcast/VariableAvailableCapacitySingleFlow",
+			senderMode: simulcastSenderMode,
+			flowMode:   singleFlowMode,
+		},
+		{
+			name:       "TestVnetRunnerSimulcast/VariableAvailableCapacityMultipleFlows",
+			senderMode: simulcastSenderMode,
+			flowMode:   multipleFlowsMode,
+		},
+	}
+
+	for _, tc := range testCases {
+		synctest.Test(t, func(t *testing.T) {
+			runner := Runner{
+				loggerFactory: lf,
+				logger:        logger,
+				name:          tc.name,
+				senderMode:    tc.senderMode,
+				flowMode:      tc.flowMode,
+			}
+			err := runner.Run()
+			assert.NoError(t, err)
+			synctest.Wait()
+		})
+	}
+
+}
